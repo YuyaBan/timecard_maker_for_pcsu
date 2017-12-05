fb6db450c82c676f5d7949667e45245b4aa374f7
YuyaBan <shepherd.1021@gmail.com>
2017-12-06 00:39:19 +0900

    module make

---
+++ data_parse.py
@@ -0,0 +1,100 @@
+# -*- coding:utf-8 -*-
+# data_parse.py
+# record:1 or 0 or 遅刻、早退
+# time_record: 全体Mtgの時間
+
+
+from datas_get import sheet_get
+
+debug = 1
+
+class datas_parse:
+
+	def search_name(list,name,length):   # 名前がある行数を取得
+		if name.count(' '):
+			print('[-] include space in serching name')
+			quit()
+		for n in range(length):
+			if list[n] == name:
+				return n
+
+	def time_get(date, length, head, sheet, row_num):   # Mtg時間を取得
+		for col_num in range(length):
+			if head[col_num].count(date):
+				st,en,rest = datas_parse.parse_time(col_num+1,sheet,row_num)
+				return (st,en,rest)
+
+		return 0
+
+	def parse_time(col_num, sheet, row_num):
+		record, record_len = sheet_get.get_col(sheet,col_num)
+
+		if debug == 1:
+			print('[+] record:{}'.format(record[row_num]))
+	
+		if record_len < row_num:
+			print('[-] record_len:{} is smaller than row_num:{}'.format(record_len,row_num))
+			print(record)
+			return('3:33','4:44','1.0')
+
+		if record[row_num] == '':												# 欠席
+			return (0,0,0)
+		elif record[row_num] == '1':
+			st,en,rest = datas_parse.time_extraction(record[record_len-1])		# 最初から最後まで出席
+			return (st,en,rest)
+		else:
+			st,en,rest = datas_parse.attendance_and_rest_time(record[row_num],record[record_len-1])	# 遅刻、早退有り
+			return (st,en,rest)
+
+	def time_extraction(time_record):
+		if debug == 1:
+			print('[+] time_record:{}'.format(time_record))
+		
+		index_kyu = time_record.find('休')
+		if index_kyu == -1:
+			print('[-] not include 休')
+			quit()
+		st = time_record[0:index_kyu-6]
+		en = time_record[index_kyu-5:index_kyu]
+		index_h = time_record.find('h')
+		if index_h == -1 or index_h < index_kyu:
+			print('[-] not include h or reverse')
+			quit()
+		rest = time_record[index_kyu+2:index_h]
+		return (st,en,rest)
+
+	def attendance_and_rest_time(record,time_record):
+		index_wave = record.find('~')
+		if index_wave == -1:
+			index_wave = record.find('～')
+		print(index_wave)
+		index_kyu = record.find('休')
+		index_h = record.find('h')
+
+		st,en,rest = datas_parse.time_extraction(time_record)
+
+		if record.count('早'):					# 早退パターン
+			en = record[index_wave+1:index_wave+6]
+			if index_kyu > 0:
+				rest = record[index_kyu+2:index_h]
+		elif 5 < index_kyu-index_wave:			# oo:oo~oo:ooパターン
+			st = record[index_wave-5:index_wave]
+			en = record[index_wave+1:index_wave+6]
+			rest = record[index_kyu+2:index_h]
+		elif index_wave < index_kyu:			# 遅刻パターン
+			st = record[index_wave-5:index_wave]
+			if index_kyu > 0:
+				rest = record[index_kyu+2:index_h]
+		elif index_wave > 0 and index_kyu == -1:# 遅刻 and 休憩不記載パターン
+			st = record[index_wave-5:index_wave]
+		else:									# 不明パターン
+			st = '3:33'
+			en = '4:44'
+			rest = '1'
+		return (st,en,rest)
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,98 +1,50 @@
 # -*- coding:utf-8 -*-
 # python main.py "<name>"
-import os
-import json
-import re
 import gspread
 import sys
+from datas_get import sheet_get
+from data_parse import datas_parse
 from oauth2client.service_account import ServiceAccountCredentials
 
 #debug flag
 debug = 1
 
-def search_name(list,name,length):
-    if name.count(' '):
-        print('[-] include space in serching name')
-        quit()
-    for n in range(length):
-        if list[n] == name:
-            return n
+def main(argvs):
 
-def del_u3000(list,length):                 # \u3000 を削除する　and 空のセルをlistから削除
-    for w in range(length):     
-        list[w] = list[w].replace('\u3000','')
-        list[w] = list[w].replace('\n','')
-        list[w] = list[w].replace(' ','')
+	wsheet = sheet_get.worksheet_acquisition(0)
 
-def del_nancell(list,length):
-    for w in range(length):
-        if list[w] == '':
-            cnt = cnt + 1
-        else:
-            cnt = 0
+	# Sheetから列情報をlistで取得
+	name_col_record, name_col_len = sheet_get.get_col(wsheet,2)
 
-        if cnt == 6:
-            st = w - 3
-            del list[st:length]
-            break
+	if debug == 1:
+		print('[+] name_col_record:{}'.format(name_col_record))
 
-def get_col(sheet,num):
-    record = sheet.col_values(num)
-    record_len = len(record)
-    del_u3000(record,record_len)
-    del_nancell(record,record_len)
-    record_len = len(record)
+	target_row_num = datas_parse.search_name(name_col_record,argvs[1],name_col_len) + 1
 
-    return (record,record_len)
+	if debug == 1:
+		print('[+] target_row_num:{}'.format(target_row_num))
 
-def get_row(sheet,num):
-    record = sheet.row_values(num)
-    del record[23:]
-    record_len = len(record)
-    del_u3000(record,record_len)
-    record_len = len(record)
+	# Sheetから行情報をlistで取得
+	target_row_record, target_row_len = sheet_get.get_row(wsheet,target_row_num)
+	head_row_record, head_row_len = sheet_get.get_row(wsheet,1)
 
-    return (record,record_len)
+	if debug == 1:
+		print('[+] head_row_record:{}'.format(head_row_record))
+		print('[+] target_row_record :{}'.format(target_row_record))
 
-def worksheet_acquisition(sheet_index):
-     # JSONをロード
-    json_path = "PCSU-timecard.json"
-    scope = ["https://spreadsheets.google.com/feeds"]
-    path = os.path.expanduser(json_path)
-    f = open('doc_id.txt')
-    doc_id = f.readline()
+	# hiduke wo list ka?
+	date = '11/29'
+	st,en,rest = datas_parse.time_get(date, head_row_len, head_row_record, wsheet, target_row_num-1)
 
-    # credentialsを取得 and Sheetの情報を取得
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(path, scope)
-    gclient = gspread.authorize(credentials)
-    gfile = gclient.open_by_key(doc_id)
-    wsheet  = gfile.get_worksheet(0) # シートのindexを任意で入力
-    return wsheet
+	if debug == 1:
+		print('[+] st:{}'.format(st))
+		print('[+] en:{}'.format(en))
+		print('[+] rest:{}'.format(rest))
 
-def main(name):
 
-    wsheet = worksheet_acquisition(0)
 
-    # Sheetから列情報をlistで取得
-    name_col_record, name_col_len = get_col(wsheet,2)
-
-    if debug == 1:
-        print('[+] name_col_record:{}'.format(name_col_record))
-
-    target_row_num = search_name(name_col_record,name[1],name_col_len) + 1
-
-    if debug == 1:
-        print('[+] target_row_num:{}'.format(target_row_num))
-
-    # Sheetから行情報をlistで取得
-    target_row_record, target_row_len = get_row(wsheet,target_row_num)
-    head_row_record, head_row_len = get_row(wsheet,1)
-
-    if debug == 1:
-        print('[+] head_row_record:{}'.format(head_row_record))
-        print('[+] target_row_record :{}'.format(target_row_record))
 
 
 if __name__ == '__main__':
-    argvs = sys.argv
-    main(argvs)
+	argvs = sys.argv
+	main(argvs)
