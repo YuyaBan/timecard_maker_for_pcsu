de7da76a279b8b94074f720d17a7f455ba71b3c5
YuyaBan <shepherd.1021@gmail.com>
2017-12-15 00:30:14 +0900

    add alldata2csv.py

---
+++ alldata2csv.py
@@ -0,0 +1,57 @@
+# -*- coding:utf-8 -*-
+# datas_get.py
+import re
+import os
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+def del_u3000(list,length):				 # \u3000 を削除する　and 空のセルをlistから削除
+	for w in range(length):
+		if isinstance(list[w],str):
+			list[w] = list[w].replace('\u3000','')
+			list[w] = list[w].replace('\n','')
+			list[w] = list[w].replace(' ','')
+
+def del_nancell(list,length):
+	for w in range(length):
+		if list[w] == '':
+			cnt = cnt + 1
+		else:
+			cnt = 0
+		if cnt == 6:
+			st = w - 5
+			del list[st:length]
+			break
+
+def worksheet_acquisition(sheet_index):
+	 # JSONをロード
+	json_path = "PCSU-timecard.json"
+	scope = ["https://spreadsheets.google.com/feeds"]
+	path = os.path.expanduser(json_path)
+	f = open('doc_id.txt')
+	doc_id = f.readline()
+
+	# credentialsを取得 and Sheetの情報を取得
+	credentials = ServiceAccountCredentials.from_json_keyfile_name(path, scope)
+	gclient = gspread.authorize(credentials)
+	gfile = gclient.open_by_key(doc_id)
+	wsheet  = gfile.get_worksheet(0) # シートのindexを任意で入力
+	return wsheet
+
+def main():
+	f = open('database.csv', 'w')
+
+	wsheet  = worksheet_acquisition(0) # シートのindexを任意で入力
+	records = wsheet.get_all_records() # head=1を指定すると便利
+
+	for record in records:
+		tmp = list(record.values())
+		length = len(tmp)
+		del_u3000(tmp,length)
+
+		for i in range(length):
+			if i != (length-1) and tmp[i] != 0: print('{},'.format(tmp[i]),end='',file=f)
+			if i == (length-1) and tmp[i] != 0: print('{}'.format(tmp[i]),file=f)
+
+if __name__ == '__main__':
+	main()
--- data_parse.py
+++ data_parse.py
@@ -6,7 +6,7 @@
 
 from datas_get import sheet_get
 
-debug = 1
+debug = 0
 
 class datas_parse:
 
@@ -19,51 +19,53 @@ class datas_parse:
 				return n
 
 	def time_get(date, length, head, sheet, row_num):   # Mtg時間を取得
+		time=[0,0,0]
 		for col_num in range(length):
 			if head[col_num].count(date):
-				st,en,rest = datas_parse.parse_time(col_num+1,sheet,row_num)
-				return (st,en,rest)
-
-		return 0
+				time = datas_parse.parse_time(col_num+1,sheet,row_num)
+				return [time[0],time[1],time[2]]
+		return [0,0,0]
 
 	def parse_time(col_num, sheet, row_num):
 		record, record_len = sheet_get.get_col(sheet,col_num)
-
+		time = [0,0,0]
 		if debug == 1:
-			print('[+] record:{}'.format(record[row_num]))
+			print('[*] record:{}'.format(record[row_num]))
 	
 		if record_len < row_num:
 			print('[-] record_len:{} is smaller than row_num:{}'.format(record_len,row_num))
 			print(record)
-			return('3:33','4:44','1.0')
+			return ['3:33','4:44','1.0']
 
 		if record[row_num] == '':												# 欠席
-			return (0,0,0)
+			return [0,0,0]
 		elif record[row_num] == '1':
-			st,en,rest = datas_parse.time_extraction(record[record_len-1])		# 最初から最後まで出席
-			return (st,en,rest)
+			time = datas_parse.time_extraction(record[record_len-1])		# 最初から最後まで出席
+			return [time[0],time[1],time[2]]
 		else:
-			st,en,rest = datas_parse.attendance_and_rest_time(record[row_num],record[record_len-1])	# 遅刻、早退有り
-			return (st,en,rest)
+			time = datas_parse.attendance_and_rest_time(record[row_num],record[record_len-1])	# 遅刻、早退有り
+			return [time[0],time[1],time[2]]
 
 	def time_extraction(time_record):
+		time=[0,0,0]
 		if debug == 1:
-			print('[+] time_record:{}'.format(time_record))
+			print('[*] time_record:{}'.format(time_record))
 		
 		index_kyu = time_record.find('休')
 		if index_kyu == -1:
 			print('[-] not include 休')
 			quit()
-		st = time_record[0:index_kyu-6]
-		en = time_record[index_kyu-5:index_kyu]
+		time[0] = time_record[0:index_kyu-6]
+		time[1] = time_record[index_kyu-5:index_kyu]
 		index_h = time_record.find('h')
 		if index_h == -1 or index_h < index_kyu:
 			print('[-] not include h or reverse')
 			quit()
-		rest = time_record[index_kyu+2:index_h]
-		return (st,en,rest)
+		time[2] = time_record[index_kyu+2:index_h]
+		return [time[0],time[1],time[2]]
 
 	def attendance_and_rest_time(record,time_record):
+		time = [0,0,0]
 		index_wave = record.find('~')
 		if index_wave == -1:
 			index_wave = record.find('～')
@@ -71,27 +73,27 @@ class datas_parse:
 		index_kyu = record.find('休')
 		index_h = record.find('h')
 
-		st,en,rest = datas_parse.time_extraction(time_record)
+		time = datas_parse.time_extraction(time_record)
 
 		if record.count('早'):					# 早退パターン
-			en = record[index_wave+1:index_wave+6]
+			time[1] = record[index_wave+1:index_wave+6]
 			if index_kyu > 0:
-				rest = record[index_kyu+2:index_h]
+				time[2] = record[index_kyu+2:index_h]
 		elif 5 < index_kyu-index_wave:			# oo:oo~oo:ooパターン
-			st = record[index_wave-5:index_wave]
-			en = record[index_wave+1:index_wave+6]
-			rest = record[index_kyu+2:index_h]
+			time[0] = record[index_wave-5:index_wave]
+			time[1] = record[index_wave+1:index_wave+6]
+			time[2] = record[index_kyu+2:index_h]
 		elif index_wave < index_kyu:			# 遅刻パターン
-			st = record[index_wave-5:index_wave]
+			time[0] = record[index_wave-5:index_wave]
 			if index_kyu > 0:
-				rest = record[index_kyu+2:index_h]
+				time[2] = record[index_kyu+2:index_h]
 		elif index_wave > 0 and index_kyu == -1:# 遅刻 and 休憩不記載パターン
-			st = record[index_wave-5:index_wave]
+			time[0] = record[index_wave-5:index_wave]
 		else:									# 不明パターン
-			st = '3:33'
-			en = '4:44'
-			rest = '1'
-		return (st,en,rest)
+			time[0] = '3:33'
+			time[1] = '4:44'
+			time[2] = '1'
+		return [time[0],time[1],time[2]]
 
 
 
